ab087534332103fea81c5631a0ee955841804660
Hypoxanthine <heyx1@shanghaitech.edu.cn>
2022-10-21 21:37:22 +0800

    Updating some tiny-nerf part

---
+++ dataset/NeRF_dataset.py
@@ -0,0 +1,75 @@
+import jittor as jt
+import numpy as np
+import os
+
+class NeRFDataset:
+    def __init__(self,
+                 data_type,
+                 root_dir,
+                 mode = 'train',
+                 batch_size = 10,
+                 ):
+        types = ['npz','nerf_synthetic','nerf_llff']
+        assert data_type in types
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.mode = mode 
+        modes = ['train','test']
+        assert self.mode in modes 
+        self.images = None
+        self.poses = None
+        self.focal_length = 0
+        self.H = 0
+        self.W = 0
+        
+        self.idx = 0
+        self.image_cnt = 0
+        
+        if (data_type == 'npz'):
+            self.load_npz_data()
+            
+        self.images = jt.array(self.images)
+        self.poses = jt.array(self.images)
+        self.focal_length = jt.array(self.focal_length)
+        
+            
+    # Use next(object) to get the new data.
+    def __next__(self):
+        assert self.batch_size < self.image_cnt
+        if self.idx+self.batch_size >=self.image_cnt:
+            self.idx = 0
+        indexs = np.arange(self.idx,self.idx+self.batch_size)
+        
+        imgs = self.images[indexs]
+        poss = self.poses[indexs]
+        self.idx += self.batch_size
+        return indexs,imgs,poss
+        
+        
+        
+        
+        
+    def load_npz_data(self):
+        # Check the type of data
+        data = np.load(self.root_dir)
+        images = data['images']
+        poses = data['poses']
+        self.focal_length = data['focal']
+        
+        self.H,self.W = images.shape[1],images.shape[2]
+        
+        if self.mode == 'train':
+            self.images = images[:100,...,:3]
+            self.poses = poses[:100]
+            self.image_cnt = 100
+        elif self.mode == 'test':
+            self.images = images[101]
+            self.poses = poses[101]
+            self.image_cnt = 1
+            
+    def reset(self):
+        self.idx = 0
+        
+        
+        
+        
---
+++ renders/NeRF_Render.py
@@ -0,0 +1,31 @@
+import numpy as np
+import jittor as jt
+
+
+class NeRF_Render:
+    def __init__(self,net,rays_o,rays_d,z,N_samples,encoding):
+        self.net = net
+        self.rays_o = jt.array(rays_o)
+        self.rays_d = jt.array(rays_d)
+        self.zmin = z[0]
+        self.zmax = z[1]
+        self.N_samples = N_samples
+        
+        # For positional encoding, it could be a function
+        # But for hash encoding, we may need a class.
+        # So we need to create a class for them.
+        self.enc = encoding
+        
+    def cumprod_exclusice(self,input):
+        dim = -1
+        cumprod = np.cumprod(input,dim)
+        cumprod = np.roll(cumprod,1,dim)
+        cumprod[...,0] = 1.0
+        return cumprod
+        
+    def render(self):
+        z_values = jt.linspace(self.zmin,self.zmax,self.N_samples)
+        
+        pts = self.rays_o[...,np.newaxis,:]+ self.rays_d[...,np.newaxis,:]*z_values[...,:,np.newaxis]
+        pts_flat = jt.reshape(pts,[-1,3])
+        
